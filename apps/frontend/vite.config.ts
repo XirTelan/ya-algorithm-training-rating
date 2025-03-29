febe3ea57f811b7a4a75db330b9667acf83a3083
Xir <prssergey@gmail.com>
2025-03-30 02:40:23 +0300

    alias config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -3,7 +3,15 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
+import path from "path";
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@features": path.resolve(__dirname, "./src/features"),
+    },
+  },
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     tailwindcss(),
