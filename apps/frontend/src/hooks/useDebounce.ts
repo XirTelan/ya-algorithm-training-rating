58faf9c64ba7cb6f10bb04015aa04bfa69658935
Xir <prssergey@gmail.com>
2025-03-30 01:48:02 +0300

    debounce hook

---
+++ apps/frontend/src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+const useDebounce = (value: string | number, ms = 300) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, ms);
+
+    return () => clearTimeout(timer);
+  }, [value, ms]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
