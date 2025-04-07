6d834641d4be0d1ba5dada30d95fd639ba29d3c3
Xir <prssergey@gmail.com>
2025-04-07 21:46:11 +0300

    feat(frontend): theme switch

--- apps/frontend/src/layout/Header.tsx
+++ apps/frontend/src/layout/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../shared/components/Nav";
+import { ModeToggle } from "@repo/ui";
 
 export default function Header() {
   return (
     <header className="backdrop-blur-sm sticky border border-b-neutral-800 w-full top-0  z-10 ">
-      <div className=" container mx-auto">
+      <div className="flex container mx-auto justify-between">
         <Nav />
+        <ModeToggle />
       </div>
     </header>
   );
