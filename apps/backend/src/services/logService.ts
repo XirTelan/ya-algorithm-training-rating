246691db09d70ba72926a231e47a3efaa29993af
Xir <prssergey@gmail.com>
2025-04-08 14:47:21 +0300

    recator(backend) request props

--- apps/backend/src/services/logService.ts
+++ apps/backend/src/services/logService.ts
@@ -19,9 +19,9 @@ async function addLogEntry(message: string, type: string) {
   }
 }
 
-async function getLogs(time: number | string, type: string) {
+async function getLogs(time: number | string, type: string = "all") {
   try {
-    const timestamp = new Date(Date.now() - Number(time) * 60 * 60 * 1000);
+    const timestamp = new Date(Date.now() - Number(time ?? 1) * 60 * 60 * 1000);
     const options: QueryOptions = { createdAt: { $gte: timestamp } };
     if (type != "all") {
       options.type = type;
